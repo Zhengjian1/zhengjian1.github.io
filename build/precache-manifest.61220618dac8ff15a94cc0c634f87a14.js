0fe65d73309992511c574ad427c44bf3ac2f6937
郑健 <zhengjian@zhengjiandeMacBook-Pro.local>
2019-01-19 17:51:41 +0800

    增加文件

---
+++ build/precache-manifest.61220618dac8ff15a94cc0c634f87a14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "268e309f2771379f6c79",
+    "url": "/static/js/main.268e309f.chunk.js"
+  },
+  {
+    "revision": "63f49301cbbd4ade6c3a",
+    "url": "/static/js/1.63f49301.chunk.js"
+  },
+  {
+    "revision": "268e309f2771379f6c79",
+    "url": "/static/css/main.7ff149bb.chunk.css"
+  },
+  {
+    "revision": "7dbe083e96ee4b709f0a422eaebdb165",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.3f1c323d3e25b0aa14ae2a5e6bb91fb6.js"
+  "/precache-manifest.61220618dac8ff15a94cc0c634f87a14.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/build/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
