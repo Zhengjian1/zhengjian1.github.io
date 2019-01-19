0ac4e12c45e55ad4e2dc747824dd5bcac89ff7b3
郑健 <zhengjian@zhengjiandeMacBook-Pro.local>
2019-01-19 16:53:34 +0800

    增加文件

--- build/precache-manifest.ccdc42a2195129db6b53d33020ea7860.js
+++ build/precache-manifest.ccdc42a2195129db6b53d33020ea7860.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/build/static/js/runtime~main.3923d73d.js"
   },
   {
-    "revision": "8b19dddcabd6878a59a9",
-    "url": "/build/static/js/main.8b19dddc.chunk.js"
+    "revision": "9b7ecb4f0f9feee7d07c",
+    "url": "/build/static/js/main.9b7ecb4f.chunk.js"
   },
   {
     "revision": "fdaecd80b7a8d09b29a0",
     "url": "/build/static/js/1.fdaecd80.chunk.js"
   },
   {
-    "revision": "8b19dddcabd6878a59a9",
-    "url": "/build/static/css/main.dca9cb65.chunk.css"
+    "revision": "9b7ecb4f0f9feee7d07c",
+    "url": "/build/static/css/main.bacb3cbe.chunk.css"
   },
   {
-    "revision": "05cdee4b7f405d1136af8973335737a5",
+    "revision": "287aaaee67e2aa24ba85bd6ffd44d0a8",
     "url": "/build/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.f075c462d4c4093f5c7fc6d006451011.js"
+  "/build/precache-manifest.ccdc42a2195129db6b53d33020ea7860.js"
 );
 
 workbox.clientsClaim();
